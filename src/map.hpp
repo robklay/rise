c4181162b4e6d2bf719b4b225e6417ecbcb6dfee
RenRokB <robklay@gmail.com>
2016-07-22 23:20:38 -0400

    Created function to convert sf::Image to Matrix

--- src/map.hpp
+++ src/map.hpp
@@ -1,7 +1,26 @@
 #pragma once
 
+#include <type_traits>
+
 #include "util/matrix.hpp"
 #include "util/types.hpp"
+
+// Creates and returns a matrix of the desired type based on an sf::Image.
+template <typename T>
+rok::Matrix<T> matrix_from_sf_image(const sf::Image& sf_image) {
+	// This function probably shouldn't support non-integral types.
+	static_assert(std::is_integral<T>);
+
+	rok::Matrix<T> matrix;
+	sf::Vector2u size = sf_image.getSize();
+
+	matrix.resize(size.x, size.y);
+	for (int i = 0; i < size.x; ++i) {
+		for (int j = 0; j < size.y; ++j) {
+			matrix.set_element(i, j, static_cast<T>(sf_image.getPixel(i, j).toInteger()));
+		}
+	}
+}
 
 class Map {
 public:
