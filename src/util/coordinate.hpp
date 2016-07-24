3f0f7c94009fd2c1745b415aa5399f68c579d6ae
RenRokB <robklay@gmail.com>
2016-07-24 17:40:55 -0400

    Fixed Coordinate class initializing and operator==

--- src/util/coordinate.hpp
+++ src/util/coordinate.hpp
@@ -5,8 +5,18 @@
 namespace rok {
 
 struct Coordinate {
-	int32 x;
-	int32 y;
+	Coordinate(int32 x, int32 y) : x(x), y(y) {}
+
+	int32 x = 0;
+	int32 y = 0;
 };
+
+inline bool operator==(Coordinate left, Coordinate right) {
+	return left.x == right.x && left.y == right.y;
+}
+
+inline bool operator!=(Coordinate left, Coordinate right) {
+	return left.x != right.x || left.y != right.y;
+}
 
 } // namespace rok
