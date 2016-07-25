6910c8241291540556f79511fc15d54600e2ef47
RenRokB <robklay@gmail.com>
2016-07-24 20:42:16 -0400

    Split coordinate.hpp into header and cpp file

---
+++ src/util/coordinate.cpp
@@ -0,0 +1,83 @@
+#include "coordinate.hpp"
+
+namespace rok {
+
+CoordinateList::CoordinateList() :
+_size(0) {}
+
+CoordinateList::CoordinateList(const std::vector<Coordinate>& coords) :
+_coords(coords),
+_size(coords.size()) {
+	if (_size > 0) {
+		_min = { coords[0].x, coords[0].y };
+		_max = { coords[0].x, coords[0].y };
+	}
+
+	reset_bounds();
+}
+
+void CoordinateList::add(const Coordinate coord) {
+	if (_size == 0) {
+		_min = { coord.x, coord.y };
+		_max = { coord.x, coord.y };
+	} else {
+		update_bounds(coord);
+	}
+
+	_coords.push_back(coord);
+}
+
+void CoordinateList::remove(const Coordinate coord) {
+	if (_size == 0) return;
+
+	auto it = std::find(_coords.begin(), _coords.end(), coord);
+	if (it == _coords.end()) return;
+
+	_coords.erase(it);
+	--_size;
+	if (_size == 0) {
+		_min = { 0, 0 };
+		_max = { 0, 0 };
+	} else {
+		reset_bounds();
+	}
+}
+
+int CoordinateList::size() const {
+	return _size;
+}
+
+Coordinate CoordinateList::min() const {
+	return _min;
+}
+
+Coordinate CoordinateList::max() const {
+	return _max;
+}
+
+const std::vector<Coordinate>& CoordinateList::coords_vector() const {
+	return _coords;
+}
+
+void CoordinateList::update_bounds(const Coordinate coord) {
+	if (coord.x < _min.x) {
+		_min.x = coord.x;
+	}
+	if (coord.y < _min.y) {
+		_min.y = coord.y;
+	}
+	if (coord.x > _max.x) {
+		_max.x = coord.x;
+	}
+	if (coord.y > _max.y) {
+		_max.y = coord.y;
+	}
+}
+
+void CoordinateList::reset_bounds() {
+	for (const Coordinate& c : _coords) {
+		update_bounds(c);
+	}
+}
+
+}
--- src/util/coordinate.hpp
+++ src/util/coordinate.hpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cassert>
+#include <vector>
 
 #include "types.hpp"
 
@@ -16,74 +17,25 @@ struct Coordinate {
 
 class CoordinateList {
 public:
-	CoordinateList() : _size(0), _min({ 0, 0 }), _max({ 0, 0 }) {}
-	CoordinateList(const std::vector<Coordinate>& coords) : _coords(coords), _size(coords.size()) {
-		if (_size > 0) {
-			_min = { coords[0].x, coords[0].y };
-			_max = { coords[0].x, coords[0].y };
-		}
+	CoordinateList();
+	CoordinateList(const std::vector<Coordinate>& coords);
 
-		reset_bounds();
-	}
+	void add(const Coordinate coord);
+	void remove(const Coordinate coord);
 
-	void add(const Coordinate coord) {
-		if (_size == 0) {
-			_min = { coord.x, coord.y };
-			_max = { coord.x, coord.y };
-		} else {
-			update_bounds(coord);
-		}
-
-		_coords.push_back(coord);
-	}
-
-	void remove(const Coordinate coord) {
-		if (_size == 0) return;
-
-		auto it = std::find(_coords.begin(), _coords.end(), coord);
-		if (it == _coords.end()) return;
-
-		_coords.erase(it);
-		--_size;
-		if (_size == 0) {
-			_min = { 0, 0 };
-			_max = { 0, 0 };
-		} else {
-			reset_bounds();
-		}
-	}
-
-	int size() const { return _size; }
+	int size() const;
 
 	// Returns a coordinate containing the lowest x and y values in the list.
-	Coordinate min() const { return _min; }
+	Coordinate min() const;
 	// Returns a coordinate containing the highest x and y values in the list.
-	Coordinate max() const { return _max; }
+	Coordinate max() const;
 
 	// Returns a reference to the underlying vector used to store the coordinates.
-	const std::vector<Coordinate>& coords_vector() const { return _coords; }
+	const std::vector<Coordinate>& coords_vector() const;
 
 private:
-	void update_bounds(const Coordinate coord) {
-		if (coord.x < _min.x) {
-			_min.x = coord.x;
-		}
-		if (coord.y < _min.y) {
-			_min.y = coord.y;
-		}
-		if (coord.x > _max.x) {
-			_max.x = coord.x;
-		}
-		if (coord.y > _max.y) {
-			_max.y = coord.y;
-		}
-	}
-
-	void reset_bounds() {
-		for (const Coordinate& c : _coords) {
-			update_bounds(c);
-		}
-	}
+	void update_bounds(const Coordinate coord);
+	void reset_bounds();
 
 	std::vector<Coordinate> _coords;
 	int _size;
